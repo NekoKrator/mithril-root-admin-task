c394aa92177a733f2dc4e0e3cb4519781f66369c
Fil Daniil <irarbc89@gmail.com>
2025-09-02 10:57:47 +0300

    feat: added Swagger API documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+
+    const config = new DocumentBuilder()
+        .setTitle('User Management')
+        .setDescription('User Management API description')
+        .setVersion('1.0')
+        .addTag('users')
+        .addBearerAuth({
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+            name: 'Authorization',
+            description: 'Enter JWT token',
+            in: 'header',
+        })
+        .build()
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, documentFactory)
+
     app.enableCors()
     await app.listen(process.env.PORT ?? 3000)
 }
--- server/src/modules/user/dto/create-user.dto.ts
+++ server/src/modules/user/dto/create-user.dto.ts
@@ -6,21 +6,34 @@ import {
     IsEnum,
 } from 'class-validator'
 import { UserRole } from '../entities/users.entity'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateUserDto {
     @IsEmail()
     email: string
 
+    @ApiProperty({
+        example: 'john.doe@mail.com',
+        required: true,
+    })
     @IsString()
     @MinLength(4)
     @MaxLength(32)
     password: string
 
+    @ApiProperty({
+        example: 'John Doe',
+        required: true,
+    })
     @IsString()
     @MinLength(4)
     @MaxLength(16)
     name: string
 
+    @ApiProperty({
+        example: 'root_admin | admin | user',
+        enum: UserRole,
+    })
     @IsEnum(UserRole)
     role: UserRole
 }
